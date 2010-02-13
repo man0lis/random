0f97dfc9c6e0d8bac49d8bd29cf08e79a4582eb9
Robin Nehls <nehls@mi.fu-berlin.de>
2010-02-13 17:09:36 +0100

    Added a fft but currently it only draws strange points

--- echo-audio.c
+++ echo-audio.c
@@ -22,18 +22,26 @@
 #include <stdlib.h>
 #include "portaudio.h"
 #include "SDL/SDL.h"
+#include "complex.h"
+#include "fftw3.h"
 #include <signal.h>
 
 typedef struct
 {
     SDL_Surface *screen;
     Uint32      color;
+    Uint32      color2;
 }
 paUserData;
 
 #define WINDOW_X 1024
 #define WINDOW_Y 200
 #define FRAMES_PER_BUFFER 1024
+
+fftw_complex *in, *out;
+fftw_plan p;
+
+volatile int stop = 0;
 
 void signalCallback(int i) {
     stop=1;
@@ -62,6 +70,7 @@ static int callMeBack(void *inputBuffer,
     float *output = (float *) outputBuffer;
     paUserData *data = (paUserData*)userData;
     unsigned int i;
+    double max;
 
     SDL_Surface *waveform;
 
@@ -83,10 +92,27 @@ static int callMeBack(void *inputBuffer,
         DrawPixel(waveform, x, y, data->color);
 
         // Set output to input
+        in[i] = *input;
         *output++ = *input++;
         // twice because we get a sterio signal
         *output++ = *input++;
     }
+    
+    fftw_execute(p);
+    max=0;
+    for(i=0;i<framesPerBuffer;i++) {
+        if (creal(out[i])>max) max = creal(out[i]);
+    }
+    
+    for(i=0;i<framesPerBuffer;i++) {
+        // make sure the x does not leave the screen
+        int x = i%WINDOW_X;
+        // calculate the y values (centered) and make sure y does not
+        // leave the screen
+        int y = abs((int)((creal(out[i])/max)*WINDOW_Y)) % WINDOW_Y;
+        // Put a pixel for the current value to the surface
+        DrawPixel(waveform, x, y, data->color2);
+    }    
 
     // Apply image to screen
     SDL_BlitSurface(waveform, NULL, data->screen, NULL);
@@ -124,7 +150,12 @@ int main(int argc, char** argv)
     paUserData data;
     data.screen = screen;
     data.color = SDL_MapRGB(screen->format,0x00,0xFF,0x00);
+    data.color2 = SDL_MapRGB(screen->format,0x00,0xFF,0xFF);
 
+    in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * FRAMES_PER_BUFFER);
+    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * FRAMES_PER_BUFFER);
+
+    p = fftw_plan_dft_1d(FRAMES_PER_BUFFER, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
 
     // var for errorCode and the stream that will be used
     PaError errorCode;
@@ -166,6 +197,8 @@ int main(int argc, char** argv)
     Pa_CloseStream(stream);
     // Kill Portaudio
     Pa_Terminate();
+
+    fftw_destroy_plan(p);
 
     // Quit SDL
     SDL_Quit();
